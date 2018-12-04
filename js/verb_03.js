9f7d785b77ceca97b1729ed74c7d901eec194fb5
Shamail <zahis538@newschool.edu>
2018-12-03 23:57:33 -0500

    Update js/verb_03.js

--- js/verb_03.js
+++ js/verb_03.js
@@ -1,3 +1,4 @@
+
 /*
   Returns a random whole number between minimum value and maximum value
   For example, calling randomNumber(0, 255);
@@ -27,17 +28,33 @@ function randomFloat (min, max) {
   return parseFloat(value.toFixed(2));
 }
 
-/* 
+/*
   Returns a color string in the form of "hsl(100, 50%, 50%)"
   For example, calling color(10, 100, 40);
   will return a string "hsl(10, 100%, 40%)"
 */
-for ( var i = 0; i < 200; i++) {
+function hslColor(h, s, l) {
+  var hue = h;
+  var saturation = s + "%";
+  var luminance = l + "%";
+
+  var color = "hsl(" + hue + "," + saturation + "," + luminance + ")";
+
+  return color;
+}
+
+for ( var i = 0; i < 500; i++) {
   // create a div
   var shape = document.createElement('div');
 
   //add class "shape" to div
   shape.classList.add('shape');
+
+  var duration = randomNumber (1000,5000);
+
+  
+  shape.style.animationDuration = duration + 'ms';
+
 
   // select container div
   var container = document.querySelector('.content');
@@ -45,6 +62,18 @@ for ( var i = 0; i < 200; i++) {
   // add a div with class "shape" to container div
   container.append(shape);
 }
+
+var shapes= document.querySelectorAll('.shape');
+
+shapes.forEach(function(shape, i) {
+  shape.addEventListener('mouseover', function(event) {
+      if(shape.classList.contains('shape-interaction')) {
+        shape.classlist.remove('shape-interaction');
+    } else {
+    shape.classList.add('shape-interaction');
+  }
+  });
+});
 
 
 
